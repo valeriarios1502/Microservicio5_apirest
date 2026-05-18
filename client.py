ff367803ad948b20bf9bffdafce89379379c4f13
valeriarios1502 <valeria.rios@utec.edu.pe>
2026-05-17 21:29:05 -0500

    fix : athena cliente

--- client.py
+++ client.py
@@ -5,21 +5,16 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-DATABASE  = os.getenv("ATHENA_DATABASE",  "glue_datalake")
-S3_OUTPUT = os.getenv("ATHENA_S3_OUTPUT", "s3://peliculas-datalake/athena-results/")
-AWS_REGION = os.getenv("AWS_REGION", "us-east-1")
+DATABASE  = os.getenv("ATHENA_DATABASE")
+S3_OUTPUT = os.getenv("ATHENA_S3_OUTPUT")
+AWS_REGION = os.getenv("AWS_REGION")
 
 athena = boto3.client("athena", region_name=AWS_REGION)
 
 
 def run_query(sql: str) -> list[dict]:
-    """
-    Ejecuta una consulta SQL en Athena con boto3 puro
-    y devuelve los resultados como lista de dicts.
-    """
     logger.info(f"▶ Ejecutando query Athena:\n{sql}")
 
-    # 1. Lanzar la query
     response = athena.start_query_execution(
         QueryString=sql,
         QueryExecutionContext={"Database": DATABASE},
@@ -27,8 +22,7 @@ def run_query(sql: str) -> list[dict]:
     )
     query_id = response["QueryExecutionId"]
 
-    # 2. Esperar resultado
-    for _ in range(60):  # máximo 60 segundos
+    for _ in range(60):  
         status_resp = athena.get_query_execution(QueryExecutionId=query_id)
         state = status_resp["QueryExecution"]["Status"]["State"]
 
@@ -42,14 +36,12 @@ def run_query(sql: str) -> list[dict]:
     else:
         raise RuntimeError("Query Athena timeout después de 60 segundos")
 
-    # 3. Obtener resultados
     results = athena.get_query_results(QueryExecutionId=query_id)
     rows = results["ResultSet"]["Rows"]
 
     if not rows:
         return []
 
-    # Primera fila = headers
     columns = [col["VarCharValue"] for col in rows[0]["Data"]]
     data = []
     for row in rows[1:]:
@@ -58,5 +50,5 @@ def run_query(sql: str) -> list[dict]:
             for i, cell in enumerate(row["Data"])
         })
 
-    logger.info(f"✅ {len(data)} filas obtenidas")
+    logger.info(f"{len(data)} filas obtenidas")
     return data
